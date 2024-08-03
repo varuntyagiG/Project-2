95e492f47441e6214aab7bc6617f039dd14daaeb
varuntyagiG <varungauravtyagi@gmail.com>
2024-08-03 20:32:48 +0530

    abc

--- Backend/Routes/SignUp.js
+++ Backend/Routes/SignUp.js
@@ -11,85 +11,109 @@ const router = express.Router();
 
 // sign up
 router.post("/signup", function (req, res) {
-  let { name, email, age, nationality, password } = req.body;
-  let Validation = UserValidation.safeParse(req.body);
-  if (Validation.success == false) {
-    return res.send("U enter wrong input");
-  }
-  bcrypt.genSalt(12, function (err, salt) {
-    bcrypt.hash(password, salt, async function (err, hash) {
-      let Entry = new User({
-        name,
-        email,
-        age,
-        nationality,
-        password: hash,
-      });
-      await Entry.save();
+  try {
+    let { name, email, age, nationality, password } = req.body;
+    let Validation = UserValidation.safeParse(req.body);
+    if (Validation.success === false) {
+      return res.send("Enter Wrong Input");
+    }
+    bcrypt.genSalt(12, function (err, salt) {
+      bcrypt.hash(password, salt, async function (err, hash) {
+        let Entry = new User({
+          name,
+          email,
+          age,
+          nationality,
+          password: hash,
+        });
+        await Entry.save();
 
-      // token generate
-      const User_id = Entry._id;
-      let token = jwt.sign({ User_id }, jwt_Secrat);
-      res.json({
-        token: token,
-        Entry,
+        // token generate
+        const User_id = Entry._id;
+        let token = jwt.sign({ User_id }, jwt_Secrat);
+        res.json({
+          token: token,
+          Entry,
+        });
       });
     });
-  });
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 });
 
 // sign in
 router.post("/signin", async function (req, res) {
-  let { email, password } = req.body;
-  let Validation_2 = SigninValidation.safeParse(req.body);
-  if (Validation_2.success === false) {
-    return res.json({
-      Message: "wrong input",
-    });
-  }
-  let SigninUser = await User.findOne({ email });
-  if (!SigninUser) {
-    return res.send("User not exist");
-  }
-  let hash = SigninUser.password;
-  let user_id = SigninUser._id;
-  bcrypt.compare(password, hash, function (err, result) {
-    if (result === true) {
-      let token = jwt.sign({ user_id }, jwt_Secrat);
-      res.json({
-        token,
+  try {
+    let { email, password } = req.body;
+    let Validation_2 = SigninValidation.safeParse(req.body);
+    if (Validation_2.success === false) {
+      return res.json({
+        Message: "wrong input",
       });
     }
-  });
+    let SigninUser = await User.findOne({ email });
+    if (!SigninUser) {
+      return res.send("User not exist");
+    }
+    let hash = SigninUser.password;
+    let user_id = SigninUser._id;
+    bcrypt.compare(password, hash, function (err, result) {
+      if (result === true) {
+        let token = jwt.sign({ user_id }, jwt_Secrat);
+        res.json({
+          token,
+        });
+      }
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 });
 
 // update route
 router.put("/update", Verification, (req, res) => {
-  let { name, email, password } = req.body;
-  let Validation = UpdateValidation.safeParse(req.body);
-  if (Validation.success === false) {
-    return res.send("Input not verified");
-  }
-  // password hassing
-  bcrypt.genSalt(12, function (err, salt) {
-    bcrypt.hash(password, salt, async function (err, hash) {
-      let update_user = await User.findByIdAndUpdate(
-        { _id: req.id },
-        { name, email, password: hash },
-        { new: true },
-      );
-      res.json(update_user);
+  try {
+    let { name, email, password } = req.body;
+    let Validation = UpdateValidation.safeParse(req.body);
+    if (Validation.success === false) {
+      return res.send("Input not verified");
+    }
+    // password hassing
+    bcrypt.genSalt(12, function (err, salt) {
+      bcrypt.hash(password, salt, async function (err, hash) {
+        let update_user = await User.findByIdAndUpdate(
+          { _id: req.id },
+          { name, email, password: hash },
+          { new: true },
+        );
+        res.json(update_user);
+      });
     });
-  });
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 });
 
 // delete route
 router.delete("/delete", Verification, async (req, res) => {
-  let deleteAccount = await User.findByIdAndDelete({ _id: req.id });
-  res.json({
-    deleteAccount,
-    message: "Account successfully deleted",
-  });
+  try {
+    let deleteAccount = await User.findByIdAndDelete({ _id: req.id });
+    res.json({
+      deleteAccount,
+      message: "Account successfully deleted",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 });
 
 module.exports = router;
